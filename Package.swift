af9d2b78119440b1e8f9680926682c513a1d1e0b
Jay Herron <NeedleInAJayStack@protonmail.com>
2026-08-09 23:20:59 -0600

    chore: Use swiftlang swift-syntax instead of apple

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/GraphQLSwift/GraphQL.git", from: "4.1.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
-        .package(url: "https://github.com/apple/swift-syntax.git", "600.0.1"..<"603.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", "600.0.1"..<"603.0.0"),
         .package(url: "https://github.com/jpsim/Yams.git", "4.0.0"..<"7.0.0"),
     ],
     targets: [
